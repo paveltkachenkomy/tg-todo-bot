5d611a2c4389fdc90ad3f10dcb9757d6a90189a2
Pavel Tkachenko <paveltkachenko.my@gmail.com>
2023-04-17 19:15:23 +0300

    Деплой на хероку

---
+++ build/common/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sayHello = void 0;
+const sayHello = () => {
+    console.log("Hello");
+};
+exports.sayHello = sayHello;
