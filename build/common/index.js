3f7e77bb247319b4d4b378e2431dacb758763d0c
Pavel Tkachenko <paveltkachenko.my@gmail.com>
2023-04-17 19:19:32 +0300

    игнор build директории

--- build/common/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sayHello = void 0;
-const sayHello = () => {
-    console.log("Hello");
-};
-exports.sayHello = sayHello;
